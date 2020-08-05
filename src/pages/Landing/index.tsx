365978cf03ed9520b2d8590e2c0e5e99de3f73d8
senaarth <senaarth@gmail.com>
2020-08-05 20:40:39 -0300

    styling landing page (mobile)

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,16 +4,20 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import './styles.css';
 
 function Landing() {
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
+
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy"/>
                     <h2>Sua plataforma de estudos online.</h2>
                 </div>
+
                 <img src={landingImg} alt="Platafor de Estudos" className="hero-image"/>
+                
                 <div className="buttons-container">
                     <a href="" className="study">
                         <img src={studyIcon} alt="Estudar"/>
