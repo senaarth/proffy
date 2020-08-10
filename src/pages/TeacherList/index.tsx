d8592c1f630454e3d2dfaaa6b2ff8afe9054d9b4
senaarth <senaarth@gmail.com>
2020-08-09 22:26:20 -0300

    styling professors list(mobile)

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.css'
 import PageHeader from '../../components/PageHeader';
+import wppIcon from '../../assets/images/icons/whatsapp.svg';
 
 function TeacherList() {
     return (
@@ -21,6 +22,29 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars1.githubusercontent.com/u/49957403?s=460&u=170776941473671902ffee948e33b4a012829359&v=4" alt="Arthur Sena"/>
+                        <div>
+                            <strong>Arthur Sena</strong>
+                            <span>Matemática</span>
+                        </div>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <footer>
+                        <p>
+                            Preço por hora:
+                            <strong>R$100,00</strong>
+                        </p>
+                        <button type="button" >
+                            <img src={wppIcon} alt="Whatsapp"/>
+                            Entrar em Contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     )
 }
