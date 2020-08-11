5c967792ef1d2df3927f111e9c6dd12d09de073c
senaarth <senaarth@gmail.com>
2020-08-11 15:38:16 -0300

    creating database

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 app.get('/', (request, response) => {
-    return response.json({ message: "Hello World!" });
+    return response.json({ message: 'Hello World!' });
 });
 app.listen(3333);
