3bdcef37ae4e7f507be2f6975a570bf1c8550eda
senaarth <senaarth@gmail.com>
2020-08-13 18:44:21 -0300

    error treatment with try/catch creation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -23,37 +23,47 @@ routes.post('/classes', async (request, response) => {
 
     const trx = await db.transaction();
 
-    const insertedUsersIds = await trx('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio
-    });
+    try {
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id
+        });
+    
+        const class_id = insertedClassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinuts(scheduleItem.from),
+                to: convertHourToMinuts(scheduleItem.to)
+            };
+        });
+    
+        await trx('class_schedule').insert(classSchedule);
+    
+        trx.commit();
+    
+        return response.status(201).send();
 
-    const user_id = insertedUsersIds[0];
+    } catch (err) {
 
-    const insertedClassesIds = await trx('classes').insert({
-        subject,
-        cost,
-        user_id
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHourToMinuts(scheduleItem.from),
-            to: convertHourToMinuts(scheduleItem.to)
-        };
-    });
-
-    await trx('class_schedule').insert(classSchedule);
-
-    trx.commit();
-
-    return response.send();
+        await trx.rollback;
+        return response.status(400).json({
+            error: 'Unexpected error while creating new class'
+        })
+        
+    }
 });
 
 export default routes;
