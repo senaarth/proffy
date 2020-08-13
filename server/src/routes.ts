56f851edd23708e32fd654195a474be5b89ba4ad
senaarth <senaarth@gmail.com>
2020-08-13 18:20:20 -0300

    inserting classes in the table

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -21,6 +21,14 @@ routes.post('/classes', async (request, response) => {
         bio
     });
 
+    const user_id = insertedUsersIds[0];
+
+    const insertedClassesIds = await db('classes').insert({
+        subject,
+        cost,
+        user_id
+    });
+
     return response.send();
 });
 
