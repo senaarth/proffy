19d847e1fc2ec4ff5aaf3a8e6a1b8b69f76cae0f
senaarth <senaarth@gmail.com>
2020-08-17 19:37:11 -0300

    creating connections controller

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+export default class ConnectionsController {
+    async index(request: Request, response: Response) {
+
+    }
+
+    async create(request: Request, response: Response) {
+
+    }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,7 @@ import ClassesController from './controllers/ClassesControllers';
 
 const routes = express.Router();
 
-const classesController = new ClassesController;
+const classesController = new ClassesController();
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
