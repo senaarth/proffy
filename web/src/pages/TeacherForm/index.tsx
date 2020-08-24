4584ee6ceae88930e419ff28df9e82bd6cb53c9d
senaarth <senaarth@gmail.com>
2020-08-24 12:14:59 -0300

    creating fieldset to teacher form page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -8,6 +8,28 @@ function TeacherForm() {
                 title="Que incrível que deseja dar aulas!"
                 description="O primeiro passo é preencher sua inscrição."
             />
+
+            <main>
+                <fieldset>
+                    <legend>Seus Dados</legend>
+
+                    <div className="input-block">
+                        <label htmlFor="name">Nome Completo</label>
+                        <input type="text" id="name" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input type="text" id="whatsapp" />
+                    </div>
+                    
+                </fieldset>
+            </main>
         </div>
     )
 }
