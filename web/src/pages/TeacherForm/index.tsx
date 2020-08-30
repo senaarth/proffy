1b61fc20842aad9df6da0e0c9b34bbdab2780436
senaarth <senaarth@gmail.com>
2020-08-30 19:42:26 -0300

    Feat: User can add more items in it's schedule.

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input/index';
 import './styles.css';
@@ -7,6 +7,17 @@ import TextArea from '../../components/TextArea';
 import Select from '../../components/Select';
 
 function TeacherForm() {
+    const [scheduleItems, setScheduleITems] = useState([
+        { week_day: 0, from: '', to: '' },
+    ]);
+
+    function addNewScheduleItem() {
+        setScheduleITems([
+            ...scheduleItems,
+            { week_day: 0, from: '', to: '' }
+        ]);
+    }
+
     return(
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -47,27 +58,31 @@ function TeacherForm() {
                 <fieldset>
                     <legend>
                         Horários Disponíveis
-                        <button>+ Novo Horário</button>
+                        <button type="button" onClick={addNewScheduleItem}>+ Novo Horário</button>
                     </legend>
 
-                    <div className="schedule-item">
-                        <Select 
-                            name="week-day" 
-                            label="Dia da Semana" 
-                            options={[
-                                { value: '0', label: 'Domingo'},
-                                { value: '1', label: 'Segunda-Feira'},
-                                { value: '2', label: 'Terça-Feira'},
-                                { value: '3', label: 'Quarta-Feira'},
-                                { value: '4', label: 'Quinta-Feira'},
-                                { value: '5', label: 'Sexta-Feira'},
-                                { value: '6', label: 'Sábado'},
-                            ]}
-                        />
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                                <Select 
+                                    name="week-day" 
+                                    label="Dia da Semana" 
+                                    options={[
+                                        { value: '0', label: 'Domingo'},
+                                        { value: '1', label: 'Segunda-Feira'},
+                                        { value: '2', label: 'Terça-Feira'},
+                                        { value: '3', label: 'Quarta-Feira'},
+                                        { value: '4', label: 'Quinta-Feira'},
+                                        { value: '5', label: 'Sexta-Feira'},
+                                        { value: '6', label: 'Sábado'},
+                                    ]}
+                                />
 
-                        <Input name="from" label="Das" type="time" />
-                        <Input name="to" label="Até" type="time" />
-                    </div>
+                                <Input name="from" label="Das" type="time" />
+                                <Input name="to" label="Até" type="time" />
+                            </div>
+                        );
+                    })}
                 </fieldset>
 
                 <footer>
