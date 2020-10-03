13e531daed18ba1f96e9605623963805fcced470
senaarth <senaarth@gmail.com>
2020-10-03 20:41:40 -0300

    Feat: Finishing filter form.

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -10,9 +11,10 @@ import styles from './styles';
 
 interface PageHeaderProps {
     title: string;
+    headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
 
     const { navigate } = useNavigation();
 
@@ -29,7 +31,11 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => {
 
                 <Image source={logoImg} resizeMode="contain" />
             </View>
-            <Text style={styles.title}>{title}</Text>
+            
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
 
             {children}
         </View>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
@@ -9,34 +10,54 @@ import TeacherItem from '../../components/TeacherItem';
 import styles from './styles';
 
 function TeacherList() {
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    function handleToggleFilterVisible() {
+        setIsFiltersVisible(!isFiltersVisible);
+    }
+
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis.">
-                <View style={styles.searchForm}>
-                    <Text style={styles.label}>Matéria</Text>
-                    <TextInput 
-                        style={styles.input}
-                        placeholder={'Escolha uma matéria.'}
-                    />
+            <PageHeader 
+                title="Proffys disponíveis." 
+                headerRight={
+                    <BorderlessButton onPress={handleToggleFilterVisible} >
+                        <Feather name="filter" size={20} color="#FFF" />
+                    </BorderlessButton>
+                }
+            >
 
-                    <View style={styles.inputGroup}>
-                        <View style={styles.inputBlock}>
-                        <Text style={styles.label}>Dia da Semana</Text>
+                { isFiltersVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Matéria</Text>
                         <TextInput 
                             style={styles.input}
-                            placeholder={'Dia da Semana'}
+                            placeholder={'Escolha uma matéria.'}
                         />
+
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Dia da Semana</Text>
+                            <TextInput 
+                                style={styles.input}
+                                placeholder={'Dia da Semana'}
+                            />
+                            </View>
+
+                            <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Dia da Semana</Text>
+                            <TextInput 
+                                style={styles.input}
+                                placeholder={'Horário'}
+                            />
+                            </View>
                         </View>
 
-                        <View style={styles.inputBlock}>
-                        <Text style={styles.label}>Dia da Semana</Text>
-                        <TextInput 
-                            style={styles.input}
-                            placeholder={'Horário'}
-                        />
-                        </View>
+                        <RectButton style={styles.submitButton}>
+                            <Text style={styles.submitButtonText}>Filtrar</Text>
+                        </RectButton>
                     </View>
-                </View>
+                )}
             </PageHeader>
 
             <ScrollView
